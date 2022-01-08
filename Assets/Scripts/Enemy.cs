52db0c3319cc004aa078a8ae15059deee817bac0
PiotrCzerwinski <86910760+PiotrCzerwinski@users.noreply.github.com>
2022-01-08 17:27:46 +0100

    Improved enemies movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,32 +2,45 @@ using UnityEngine;
 using System.Collections;
 
 public class Enemy : MonoBehaviour
-{   
-    
+{
+    private Rigidbody rb;
     private GameObject target;
     [SerializeField]
-    public float movementSpeed = 3f;
+    public float movementSpeed = 0.5f;
     [SerializeField]
     public float health = 3f;
+    private Vector3 forward;
 
     public float minDistance = 2f;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, target.transform.position) >= minDistance) {
-            moveToPlayer();
-        }
+        /* if (Vector3.Distance(transform.position, target.transform.position) >= minDistance)
+         {
+             moveToPlayer();
+         }*/
+        //moveToPlayer();
+    }
+    private void FixedUpdate()
+    {
+        moveToPlayer();
     }
 
     public void moveToPlayer() {
 
-        transform.LookAt(target.transform.position);
-        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        Vector3 playerPosition = Vector3.MoveTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime);
+        //rb.velocity += transform.forward * movementSpeed;
+        rb.MovePosition(playerPosition);
+        transform.LookAt(target.transform);
     }
 
     public void takeDamage() {
