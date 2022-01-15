fed9748969aba86eb2725a8a0a781a6a79979c27
PiotrCzerwinski <86910760+PiotrCzerwinski@users.noreply.github.com>
2022-01-15 13:38:46 +0100

    Added mountains, more trees and camera zoom on scroll wheel

--- Assets/Scripts/CameraPosition.cs
+++ Assets/Scripts/CameraPosition.cs
@@ -6,6 +6,10 @@ public class CameraPosition : MonoBehaviour
 {
     public Transform player;
     private Camera cam;
+    private float minCameraSize = 3f;
+    private float maxCameraSize = 14f;
+    private float cameraSize = 4f;
+    private float scrollSpeed = 2f;
     [SerializeField]
     public Vector3 offset;
     void Start()
@@ -20,6 +24,8 @@ public class CameraPosition : MonoBehaviour
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.transform.position + offset;
-        
+        cameraSize += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        cameraSize = Mathf.Clamp(cameraSize, minCameraSize, maxCameraSize);
+        cam.orthographicSize = cameraSize;
     }
 }
